e931489e60a03606e72a9b879ff4aaa08b8e97cd
mrjailman <mr.jailmann@gmail.com>
2018-08-05 19:15:14 +0300

    step2

--- 1st task/index.php
+++ 1st task/index.php
@@ -1,6 +1,14 @@
 <?php
 //created by Yehor Sivash"
-$a="My first project";
-var_dump($a);
+//$a="My first project";
+//var_dump($a);
+    echo        "www-world wide web";
+     echo        "<br>domain = «зона», «область»";
+      echo          "<br>Сервер – это компьютер!";
+       echo         "<br>Что такое Хостинг?-Это место на компьютере, с которого скачиваются файлы";
+
+
+
+
 
 ?>
