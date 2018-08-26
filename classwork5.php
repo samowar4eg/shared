1d62eb88d5007bfb363a17bd7478e0eada005f64
mrjailman <mr.jailmann@gmail.com>
2018-08-26 11:08:04 +0300

    1

---
+++ classwork5.php
@@ -0,0 +1,40 @@
+<?php
+//function add_some_extra(&$string)
+//{
+//    $string.='и кое что еще';
+//}
+//$str='это строка,';
+//add_some_extra($str);
+//echo $str;
+
+//function makecoffe($type="capuccino")
+//{
+//    return "Make a cup of $type.\n";
+//}
+//echo makecoffe();
+//echo makecoffe(null);
+//echo makecoffe("espresso");
+
+//
+//function makecoffee($types=array("capuccino"),$coffeeMaker=null)
+//{
+//    $device=is_null($coffeeMaker)?"manual":$coffeeMaker;
+//    return"Make cup".join(",",$types)."$device. \n";
+//}
+//echo makecoffee();
+//echo makecoffee(array("capucino","lavazza"),"in kettle");
+
+//function maketea($type ="green" ,$flavour)
+//{
+//    return "make cup $type of tea with $flavour taste.";
+//}
+//echo maketea("flavor");
+
+//function add($a,$b)
+//{
+//    return $a+$b;
+//}
+//echo add(...[1,2])."\n";
+//$a=[1,2];
+//echo add(...$a);
+
